dcd54b275d301b28cce3ff7bfc98a7dee2d74def
Robert Pelayo <robpelayo@gmail.com>
2020-06-08 13:03:30 -0700

    Added totals to food items

---
+++ FinalProject/remove_state.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, redirect, url_for
+from flask.views import MethodView
+import gbmodel
+
+
+class Remove_State(MethodView):
+    def get(self):
+        return render_template('remove_state.html')
+
+    def post(self):
+        state_to_remove = request.form['state'].title()
+        print(state_to_remove)
+        model = gbmodel.get_model()
+        if model.delete(state_to_remove):
+            return redirect(url_for('index'))
+        else:
+            return render_template('error.html')
